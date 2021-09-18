eb779f6b26dff6cae2059e7f972ce6bb99032ef8
P Yeshashwini <yeshashwiniec@gmail.com>
2021-09-18 17:51:25 +0530

    Committing Files - Message

--- src/calculator.java
+++ src/calculator.java
@@ -1,47 +1,20 @@
 import java.util.Scanner;
 
 public class calculator {
+	static int output;
+	static int output1;
+	public static void multiply() {
+		output=5*6;
+	}
+	public static void division() {
+		output1=5/6;
+	}
 
     public static void main(String[] args) {
 
-    	double num1, num2;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first number:");
-
-        /* We are using data type double so that user
-         * can enter integer as well as floating point
-         * value
-         */
-        num1 = scanner.nextDouble();
-        System.out.print("Enter second number:");
-        num2 = scanner.nextDouble();
-
-        System.out.print("Enter an operator (*, /): ");
-        char operator = scanner.next().charAt(0);
-
-        scanner.close();
-        double output;
-
-        switch(operator)
-        {
-
-            case '*':
-            	output = num1 * num2;
-                break;
-
-            case '/':
-            	output = num1 / num2;
-                break;
-
-            /* If user enters any other operator or char apart from
-             * +, -, * and /, then display an error message to user
-             * 
-             */
-            default:
-                System.out.printf("You have entered wrong operator");
-                return;
-        }
-
-        System.out.println("Ouput: "+output);
+    	multiply();
+    	division();
+    	System.out.println(output);
+    	System.out.println(output1);
     }
 }
