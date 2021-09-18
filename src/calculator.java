182d68e33d43db105b11f4b8d73bbb8601abce49
P Yeshashwini <yeshashwiniec@gmail.com>
2021-09-18 18:01:20 +0530

    initial commit

--- src/calculator.java
+++ src/calculator.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 
 public class calculator {
-	static int output;
-	static int output1;
-	public static void multiply() {
-		output=5*6;
-	}
-	public static void division() {
-		output1=5/6;
-	}
 
-    public static void main(String[] args) {
+    private static int output;
+	private static int output2;
+
+	public static void main(String[] args) {
 
     	multiply();
     	division();
     	System.out.println(output);
-    	System.out.println(output1);
-    }
+    	System.out.println(output2);
+    	
+}
+
+	private static void division() {
+		// TODO Auto-generated method stub
+		 output = 5+6;
+		
+	}
+
+	private static void multiply() {
+		// TODO Auto-generated method stub
+		 output2 = 10/2;
+	}
 }
